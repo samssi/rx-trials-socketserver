4dcd70c3c5c368613eb886927de4d3286e7cabad
Sämir Hamadi <samir.hamadi@gmail.com>
2017-06-22 00:13:17 +0300

    Server can now emit messages from console

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 "use strict";
 
-const app = require('http').createServer();
-const io = require('socket.io')(app);
-const fs = require('fs');
+const app = require("http").createServer();
+const io = require("socket.io")(app);
+const fs = require("fs");
+const keypress = require("keypress");
+const readline = require('readline');
+
+const lineReader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
 
 app.listen(8001);
+keypress(process.stdin);
 
 io.on("connection", function (socket) {
     console.log("New connection opened...");
     socket.emit("message", { message: "Message from server!" });
+    lineReader.on("line", (line) => {
+        console.log("Sending: %s", line);
+        socket.emit("message", { message: line });
+    });
     socket.on("disconnect", () => {console.log("Connection closed...")});
-
 });
+
+
+
